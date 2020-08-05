0a57e4ccffe9e92a2546a7c7f0ab3a2145f8351c
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-06 01:09:00 +0530

    Create PLAYGAME.cpp

---
+++ PLAYGAME.cpp
@@ -0,0 +1,20 @@
+//PLAYGAME - PLAYGAME
+
+#include <iostream>
+using namespace std;
+int main() {
+int t;
+unsigned long long int n;
+cin>>t;
+while(t--)
+{
+	cin>>n;
+	if(n%3==1)
+	cout<<"Hemlata\n";
+	else if(n%3==2)
+	cout<<"Hemlata\n";
+	else
+	cout<<"Ragini\n";
+}
+return 0;
+}

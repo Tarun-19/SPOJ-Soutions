f8392329c264a31f15f927ce33274f374c4e75f2
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-07 01:37:06 +0530

    Create CRZYSMKR.py

---
+++ CRZYSMKR.py
@@ -0,0 +1,11 @@
+# CRZYSMKR - Crazy Smoker
+
+from sys import stdin, stdout
+# t=int(raw_input())
+t=int(stdin.readline())
+while(t>0):
+    t=t-1
+    # n=int(raw_input())
+    n=int(stdin.readline())
+    n=(11-((n%11)*8)%11)%11
+    print n

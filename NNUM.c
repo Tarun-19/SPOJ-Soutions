6f419140ac5403cfddc2dd0787d065f0f3c72536
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 01:50:28 +0530

    Create NNUM.c

---
+++ NNUM.c
@@ -0,0 +1,12 @@
+// NNUM - One of the Simpsons symbols
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <stdio.h>
+int main() {
+	int a[]={0,9,0,4,3,3,1,4,3,4,1,2};
+	scanf("%d",&a[11]);
+	printf("%d",a[a[11]]);
+	return 0;
+}

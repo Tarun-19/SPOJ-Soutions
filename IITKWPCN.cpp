037a2a7850c9231866f48d82a8f7d242666374d3
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 02:02:58 +0530

    Create IITKWPCN.cpp

---
+++ IITKWPCN.cpp
@@ -0,0 +1,20 @@
+// IITKWPCN - Playing With Balls
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <iostream>
+using namespace std;
+int main() {
+	long int n,t;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>n;
+	    if(n%2)
+	    cout<<"1.000000\n";
+	    else
+	    cout<<"0.000000\n";
+	}
+	return 0;
+}

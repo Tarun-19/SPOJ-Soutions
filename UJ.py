279cbf759ffcb7fe1fab12c11d1c96316cbf16bb
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 13:07:59 +0530

    Create UJ.py

---
+++ UJ.py
@@ -0,0 +1,10 @@
+# UJ - Uncle Jack
+
+# Author: Tarun Kumar
+# E-mail: tarunkumar281200@gmail.com
+
+while(1):
+    n, x=map(int, raw_input().split())
+    if(n==0 and x==0):
+        break
+    print(pow(n,x))

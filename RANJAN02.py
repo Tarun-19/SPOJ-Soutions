407cf2695f3546283ce11be2b5d238d8d6a88bf3
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 15:19:07 +0530

    Create RANJAN02.py

---
+++ RANJAN02.py
@@ -0,0 +1,11 @@
+# RANJAN02 - Tower Of Hanoi - Revisited
+
+# Author: Tarun Kumar
+# E-mail: tarunkumar281200@gmail.com
+
+t=int(input())
+while t>0:
+ n=int(input())
+ n=(3**n)-1
+ print (n)
+ t=t-1

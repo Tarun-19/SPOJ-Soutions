a44129231074090633ac9e9b34039e41eac450ea
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 17:30:02 +0530

    Create MINCOUNT.cpp

---
+++ MINCOUNT.cpp
@@ -0,0 +1,17 @@
+// MINCOUNT - Move To Invert
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <iostream>
+using namespace std;
+int main() {
+	unsigned long long int t,h;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>h;
+	    cout<<((h*h+h)/6)<<endl;
+	}
+	return 0;
+}

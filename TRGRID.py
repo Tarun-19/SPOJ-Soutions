6421c5e6e07d94f9cc24c999a435b478624f09b0
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 13:09:36 +0530

    Create TRGRID.py

---
+++ TRGRID.py
@@ -0,0 +1,20 @@
+// TRGRID - Traversing Grid
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+t=int(input())
+while(t>0):
+    t=t-1
+    n, m=map(int,input().split())
+    if (n<=m):
+        if(n%2==0):
+            print('L')
+        else:
+            print('R')
+    else:
+        if(m%2==0):
+            print('U')
+        else:
+            print('D')
+            

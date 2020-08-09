162d317827472a0483665d25a90c2c29f811aafc
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 13:33:34 +0530

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,11 @@
+# TEST - Life, the Universe, and Everything
+
+# Author: Tarun Kumar
+# E-mail: tarunkumar281200@gmail.com
+
+while(True):
+    n=int(input())
+    if(n==42):
+        break
+    else:
+        print(n)

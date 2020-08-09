cd01f51459e8dad320d9d6a18186634c3096ec23
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 16:28:36 +0530

    Create SHAKTI.cpp

---
+++ SHAKTI.cpp
@@ -0,0 +1,20 @@
+// SHAKTI - SHAKTIMAN AND KILWISH
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <iostream>
+using namespace std;
+int main() {
+	long int t,n;
+	cin>>t;
+	while(t--)
+	{
+	        cin>>n;
+         	if(n%2==0)
+	        cout<<"Thankyou Shaktiman\n";
+	        else
+	        cout<<"Sorry Shaktiman\n";
+    }
+	return 0;
+}

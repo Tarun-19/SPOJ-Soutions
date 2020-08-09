46628603686717794fecc0243870b9c92e63ea75
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 17:25:46 +0530

    Create MAXLN.java

---
+++ MAXLN.java
@@ -0,0 +1,20 @@
+// MAXLN - THE MAX LINES
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+import java.util.*;
+import java.lang.*;
+class GFG {
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    long r;
+	    int t,i;
+	    t=sc.nextInt();
+	    for(i=1;i<=t;i++)
+	    {
+	        r=sc.nextInt();
+	        r=4*r*r;
+		    System.out.println("Case "+i+": "+r+".25");
+	}
+}}

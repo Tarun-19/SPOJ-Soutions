ace5e006c028022881f8d0fc42e1a32fc99d524d
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 13:18:07 +0530

    Create VFMUL.py

---
+++ VFMUL.py
@@ -0,0 +1,11 @@
+# VFMUL - Very Fast Multiplication
+
+# Author: Tarun Kumar
+# E-mail: tarunkumar281200@gmail.com
+
+import math
+t=int(input())
+while(t>0):
+    t=t-1
+    n, x=map(int, raw_input().split())
+    print(n*x)

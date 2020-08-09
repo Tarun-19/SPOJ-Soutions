40b3eb267b1a05b69f4d4b6040cfae2f1ec5352c
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 13:15:23 +0530

    Create YAPP.py

---
+++ YAPP.py
@@ -0,0 +1,13 @@
+# YAPP - Yet Another Permutations Problem
+
+# Author: Tarun Kumar
+# E-mail: tarunkumar281200@gmail.com
+
+import math
+t=int(input())
+while(t>0):
+    t=t-1
+    n=int(input())
+    n=n-1
+    n=pow(2,n,1000000007)
+    print n

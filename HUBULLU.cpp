688c48de7b796ae97646c52d7f6c76d60c80cd92
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 21:53:55 +0530

    Create HUBULLU.cpp

---
+++ HUBULLU.cpp
@@ -0,0 +1,20 @@
+// HUBULLU - Hubulullu
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <iostream>
+using namespace std;
+int main() {
+    long long int n,m,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        if(m==0)
+        cout<<"Airborne wins.\n";
+        else
+        cout<<"Pagfloyd wins.\n";
+    }
+	return 0;
+}

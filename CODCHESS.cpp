d615782a87c67cae64d43231ddc52b1e632e3f7e
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-08-09 19:57:11 +0530

    Create CODCHESS.cpp

---
+++ CODCHESS.cpp
@@ -0,0 +1,21 @@
+// CODCHESS - Naya Shatranj (New Chess)
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n%2==0)
+	    cout<<"1\n";
+	    else
+	    cout<<"0\n";
+	}
+	return 0;
+}

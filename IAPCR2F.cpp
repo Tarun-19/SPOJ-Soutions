e32309041cf914f1c84455424d26c18410ee48e1
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-09-17 23:36:27 +0530

    Create IAPCR2F.cpp

---
+++ IAPCR2F.cpp
@@ -0,0 +1,101 @@
+// IAPCR2F - Summer Trip
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define INOUT freopen(name".inp","r", stdin); freopen(name".out","w",stdout);
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll,ll>>
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define mp make_pair
+#define ff first
+#define ss second
+#define bs binary_search
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define f0(i,n) for(i=0;i<n;i++)
+#define f1(i,n) for(i=1;i<=n;i++)
+#define fab(i,a,b) for(i=a;i<=b;i++)
+#define f0r(i,n) for(i=n-1;i>=0;i--)
+#define f1r(i,n) for(i=n;i>=1;i--)
+#define fabr(i,a,b) for(i=b;i>=a;i--)
+#define memo(a,b) memset(a,b,sizeof(a))
+#define lcm(x,y) boost::math::lcm(x,y)
+#define display(x) for(auto dsp:x)cout<<dsp<<" ";
+#define hi cout<<"hi\n";
+#define ln "\n"
+using namespace std;
+
+ll vis[10000]={0};
+vll adj[10000];
+vll vec;
+ll amt,a[10000];
+
+void dfs(ll n)
+{
+    vis[n]=1;
+    amt+=a[n];
+    
+    for(auto child:adj[n])
+    {
+        if(vis[child]==0)
+        dfs(child);
+    }
+}
+
+int main()
+{
+    FAST;
+    ll t,n,i,j,k,len,x,y,z,c,f,flag,p,q,mx,mn,l,r,sum,ans,tmp,it,pos,avg,m;
+    cin>>t;
+    it=1;
+    while(t--)
+    {
+        vec.clear();
+        f0(i,10000)
+        {
+            adj[i].clear();
+            vis[i]=0;
+        }
+        cin>>n>>p;
+        ll u,v;
+        ll cnt=0;
+        
+        f1(i,n)
+        cin>>a[i];
+        
+        while(p--)
+        {
+            cin>>u>>v;
+            adj[u].pb(v);
+            adj[v].pb(u);
+        }
+        
+        
+        fab(i,1,n)
+        {
+            if(vis[i]==0)
+            {
+                amt=0;
+                dfs(i);
+                vec.pb(amt);
+            }
+        }
+        
+        cout<<"Case "<<it++<<": ";
+        cout<<vec.size()<<ln;
+        sort(all(vec));
+        display(vec);
+    }
+	return 0;
+}

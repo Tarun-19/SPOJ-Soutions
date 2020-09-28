109634f72c381698b984f108fc6b24c581271277
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-09-29 01:12:31 +0530

    Create ARRTWIST.cpp

---
+++ ARRTWIST.cpp
@@ -0,0 +1,33 @@
+// ARRTWIST - TWISTED ARRAY
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define f0(i,n) for(i=0;i<n;i++)
+#define f1(i,n) for(i=1;i<=n;i++)
+#define fab(i,a,b) for(i=a;i<=b;i++)
+#define fabr(i,a,b) for(i=b;i>=a;i--)
+using namespace std;
+int main()
+{
+    FAST;
+    ll t,n,i,j,k,l,p,q,r,ans,avg,sum;
+    // cin>>t;
+    t=1;
+    while(t--)
+    {
+        cin>>n>>k;
+        
+        fab(i,0,n+k-1)
+        cin>>l;
+        
+        
+        cout<<"Yes";
+        
+        
+    }
+	return 0;
+}

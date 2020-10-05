e1e4d50ac3175c965f49ee3e95ea729733509392
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-10-05 14:31:40 +0530

    Create ACT.cpp

---
+++ ACT.cpp
@@ -0,0 +1,21 @@
+// ACT - Alpha Centauri Tennis
+
+// Author: Tarun Kumar
+// E-mail: tarunkumar281200@gmail.com
+
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ll i,t,j,n,k,x,y,sx,sy;
+    cin>>t;
+    for(k=1;k<=t;k++)
+    {
+        string s;
+        cin>>n>>s;
+        
+        cout<<s.back()<<endl;
+    }
+	return 0;
+}

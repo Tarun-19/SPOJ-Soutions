bcaca247765128e4b47817bb02371d1daa541fec
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2020-10-17 01:40:45 +0530

    Create EXPECT.lua

---
+++ EXPECT.lua
@@ -0,0 +1,11 @@
+// EXPECT - Life, the Universe, and Everything (Interactive)
+
+// Author: cf:Tarun_19/cc:tarun_19_
+// E-mail: tarunkumar281200@gmail.com
+
+t=0
+while t~=42 do
+    num = io.read()
+    print(num)
+    t=tonumber(num)
+end
